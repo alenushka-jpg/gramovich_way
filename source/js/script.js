3929dd70137f555513ee7f2c53a7aabf4b121ba6
alenushka <alionxlo@gmail.com>
2021-10-20 11:00:57 +0300

    Добавляет открытое состояние хедера при no-js, правит разметку

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,31 @@
-import './modules/burger.js'
+// Переключение выпадающего меню
+const pageHeader = document.querySelector('.page-header');
+const burger = document.querySelector('.page-header__burger');
+const navigation = document.querySelector('.main-navigation');
+const navigationClose = document.querySelector('.main-navigation__close');
+const navigationLinks = document.querySelectorAll('.main-navigation__link');
+
+pageHeader.classList.remove('no-js');
+
+function showNavigation() {
+  navigation.classList.add('main-navigation--open');
+
+  navigationLinks.forEach((link) => {
+    link.addEventListener('click', hideNavigation);
+  });
+};
+
+function hideNavigation() {
+  navigation.classList.remove('main-navigation--open');
+};
+
+const onBurgerClick = () => {
+  showNavigation();
+};
+
+const onCloseClick = () => {
+  hideNavigation();
+};
+
+burger.addEventListener('click', onBurgerClick);
+navigationClose.addEventListener('click', onCloseClick);
