e7738dca56e678a4d2a19ae696d81f1880741782
alenushka <alionxlo@gmail.com>
2022-08-29 17:53:27 +0300

    Вносит правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,17 @@
-// Переключение выпадающего меню
+
 const pageHeader = document.querySelector('.page-header');
 const burger = document.querySelector('.page-header__burger');
 const navigation = document.querySelector('.main-navigation');
 const navigationClose = document.querySelector('.main-navigation__close');
 const navigationLinks = document.querySelectorAll('.main-navigation__link');
+const overlay = document.querySelector('.overlay');
+const modalWindow = document.querySelector('.modal-window');
+const modalOpen = document.querySelectorAll('.js-open-popup');
+const modalClose = document.querySelector('.js-close-popup');
 
 pageHeader.classList.remove('no-js');
 
+// Переключение выпадающего меню
 function showNavigation() {
   navigation.classList.add('main-navigation--open');
 
@@ -31,10 +36,6 @@ burger.addEventListener('click', onBurgerClick);
 navigationClose.addEventListener('click', onCloseClick);
 
 //Подключение модального окна
-const modalWindow = document.querySelector('.modal-window');
-const modalOpen = document.querySelectorAll('.js-open-popup');
-const modalClose = document.querySelector('.js-close-popup');
-
 function showModal() {
   modalWindow.classList.add('modal-window--show');
 }
@@ -56,6 +57,19 @@ modalOpen.forEach((item) => {
 })
 
 modalClose.addEventListener('click', onModalCloseClick);
+
+if (overlay) {
+  overlay.addEventListener('click', hideModal);
+}
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalWindow.classList.contains('modal-window--show')) {
+      evt.preventDefault();
+      modalWindow.classList.remove('modal-window--show');
+    }
+  }
+});
 //Переключение карточек страны
 
 const tabsSwitch = document.querySelectorAll('.countries__toggle');
